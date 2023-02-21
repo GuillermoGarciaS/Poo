c32fc99133d2b39ec867d066bbce2c1820cbfba1
GuillermoGarciaS <eivengamer11roblox@gmail.com>
2023-02-20 20:29:44 -0600

    Update Reto3

--- edu.Guillermo.Garcia.Sainz/Reto3/process/ConversorNumerico.java
+++ edu.Guillermo.Garcia.Sainz/Reto3/process/ConversorNumerico.java
@@ -1,3 +1,131 @@
+/**
+ *
+ * esta clase contiene metodos para convertir numeros
+ */
+
 public class ConversorNumerico {
+    /**
+      * aqui se encuentras cada de las operaciones que se usaran en el CLI
+      * para que el usuario tenga su resultado
+      */
+ 
+      public static String convertidorDecimalABinario(String DecimalABinario) {
+        int decimal = 0;
+        int residuo;
+        String binario = "";
+
+        while (decimal > 0) {
+
+            residuo = (decimal % 2);
+            binario = residuo + binario;
+            decimal = (decimal / 2);
+        }
+        return binario;
+    }
+
+    public static String convertidorDecimalAOctal(int DecimalAOctal) {
+        String octal = "";
+        String ranOctal = "12345670";
+
+        if (DecimalAOctal == 0) {
+           octal = "0";
+        }
+
+        while (DecimalAOctal > 0) {
+
+            int residuo = DecimalAOctal % 8;
+            octal = (ranOctal.charAt(residuo) + octal);
+            DecimalAOctal /= 8;
+
+        }
+        return octal;
+    }
+
+    public static String convertidorDecimalHexadecimal(int DecimalAHexadecimal) {
+        String hexadecimal = "";
+        String ranhexadeccimal = "1234567890ABCDEF";
+
+        if (DecimalAHexadecimal == 0) {
+           hexadecimal = "0";
+        }
+
+        while (DecimalAHexadecimal > 0) {
+            int residuo = DecimalAHexadecimal % 16;
+            hexadecimal = (ranhexadeccimal.charAt(residuo) + hexadecimal);
+            DecimalAHexadecimal /= 16;
+        }
+        return hexadecimal;
+    }
+
+    public static long convertidorBinarioADecimal(String BinarioADecimal) {
+        long decimal = 0;
+        int exponente = 0;
+
+        for (int i = BinarioADecimal.length() - 1; i >= 0; i--) {
+            short digito = 1;
+            if (BinarioADecimal.charAt(i) == '0') {
+                digito = 0;
+            }
+
+            double multiplicador = Math.pow(2, exponente);
+            decimal += digito * multiplicador;
+            exponente++;
+        }
+        return decimal;
+       }
+
+    public static int convertidorOctalADecimal(String DecimalAOctal) {
+        int octal = Integer.parseInt(DecimalAOctal);
+        int exponente = 1;
+        int decimal = 0;
+
+        if (octal == 0) {
+            decimal = 0;
+        }
+
+        while (octal > 0) {
+            int residuo = (octal % 10);
+            decimal = (decimal + (residuo * exponente));
+            octal = octal / 10;
+            exponente = exponente * 8;
+        }
+        return octal;
+    }
+
+    /**
+     *
+     * aqui la unica diferencia del resto, es que usa un switch para caracteres de char,
+     * esto ya que el Hexadecimal contiene letras de la A hasta la F
+     */
     
+    public static int HexadecimalADecimal(String HexadecimalADecimal) {
+       HexadecimalADecimal.toUpperCase();
+       int hexadecimal = Integer.parseInt(HexadecimalADecimal);
+       int exponente = 1;
+       int decimal = 0;
+
+       while (hexadecimal > 0) {
+           int residuo = (hexadecimal % 10);
+           decimal = (decimal + (residuo * exponente));
+           hexadecimal = hexadecimal / 10;
+           exponente = exponente * 8;
+       }
+
+        switch (HexadecimalADecimal) {
+            case "A":
+                return 10;
+            case "B":
+                return 11;
+            case "C":
+                return 12;
+            case "D":
+                return 13;
+            case "E":
+                return 14;
+            case "F":
+                return 15;
+            default:
+                return Integer.parseInt(String.valueOf(HexadecimalADecimal));
+        }
+    }
 }
--- edu.Guillermo.Garcia.Sainz/Reto3/ui/CLI.java
+++ edu.Guillermo.Garcia.Sainz/Reto3/ui/CLI.java
@@ -1,3 +1,92 @@
 public class CLI {
+    /*
+     * 
+     * Aqui se ejecutara y mostrara el menu
+     * 
+     */
+
+     public static void Main(Object ConversorNumerico) {
+
+        String DecimalABinario = "";
+        int DecimalAOctal = 0;
+        int DecimalAHexadecimal = 0;
+        String BinarioADecimal = "";
+        String OctalADecimal = "";
+        String HexadecimalADecimal = "";
+        int salir = 0;
+
+        salida:
+
+        do {
+            System.out.println("Bienvenido! seleccione que tipo de opccion usara");
+            System.out.println("/1/ Decimal a Binario");
+            System.out.println("/2/ Decimal a Octal");
+            System.out.println("/3/ Decimal a Hexadecimal");
+            System.out.println("/4/ Binario a Decimal");
+            System.out.println("/5/ Octal a Decimal");
+            System.out.println("/6/ Hexadecimal a Decimal");
+            System.out.println("/7/ Salir");
+
+            try (Scanner scanner1 = new Scanner(System.in)) {
+                int opccion = scanner1.nextInt();
+
+                while (opccion > 7 && opccion < 1) {
+                    System.out.println("Digito invalido, porfavor intente de nuevo");
+                    opccion = scanner1.nextInt();
+                }
+
+                if (opccion == 7) {
+                    System.out.println("Tenga un buen día");
+                    break salida;
+
+                    /*
+                     *
+                     * aqui el switch traera las operaciones usadas en "Process", despues usando
+                     * un break para terminarla
+                     */
+
+                }
+                switch (opccion) {
+
+                    case 1:
+                        System.out.println("Ingrese el digito(s)");
+                        String numero = scanner1.next();
+                        System.out.println("La conversion ressulta en " + DecimalABinario);
+                        break;
+                    case 2:
+                        System.out.println("Ingrese el digito(s)");
+                        String numero2 = scanner1.next();
+                        System.out.println("La conversion ressulta en " + DecimalAOctal);
+                        break;
+                    case 3:
+                        System.out.println("Ingrese el digito(s)");
+                        String numero3= scanner1.next();
+                        System.out.println("La conversion ressulta en " + DecimalAHexadecimal);
+                        break;
+                    case 4:
+                        System.out.println("Ingrese el digito(s)");
+                        String numero4 = scanner1.next();
+                        System.out.println("La conversion ressulta en " + BinarioADecimal);
+                        break;
+                    case 5:
+                        System.out.println("Ingrese el digito(s)");
+                        String numero5 = scanner1.next();
+                        System.out.println("La conversion ressulta en " + OctalADecimal);
+                        break;
+                    case 6:
+                        System.out.println("Ingrese el digito(s)");
+                        String numero6 = scanner1.next();
+                        System.out.println("La conversion ressulta en " + HexadecimalADecimal);
+                        break;
+                    case 7:
+                        salir++;
+                        break;
+                }
+            }
+        }
+        while (salir == 0);
+    }
     
+    public static void mostrarMenu() {
+    }
 }
