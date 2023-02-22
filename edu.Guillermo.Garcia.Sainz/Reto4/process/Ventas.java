14fd220074a29238243b76cf4415b0ae0f3d7742
GuillermoGarciaS <eivengamer11roblox@gmail.com>
2023-02-21 21:50:17 -0600

    Reto4 Final

--- edu.Guillermo.Garcia.Sainz/Reto4/process/Ventas.java
+++ edu.Guillermo.Garcia.Sainz/Reto4/process/Ventas.java
@@ -1,7 +1,20 @@
+/*
+ * 
+ * Generar metedos que obtienen el producto que más se vende por trimestre y por sabor
+ * 
+ */
+
 package edu.Guillermo.Garcia.Sainz.Reto4.process;
 import java.util.ArrayList;
 
 public class Ventas {
+
+    /*
+     * 
+     * Se leen los valores de una tabla, posterior a esto regresa el
+     * valor máximo de cada fila 
+     * 
+     */
     
     public static ArrayList<Integer> MaxProduct(double [][] tabla) {
 
@@ -21,6 +34,13 @@ public class Ventas {
         }
         return maximo;
     }
+
+    /*
+     * 
+     * Aqui se sumaran los valores para devolver la posicion
+     * y el valor más grande
+     * 
+     */
 
     public static int maxTrimeste(double[][] tabla) {
         int maxTrimeste = 0;
