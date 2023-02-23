57a520803594d97e5aebedc8b175ef16f8b3738f
GuillermoGarciaS <eivengamer11roblox@gmail.com>
2023-02-22 22:49:49 -0600

    Reto5 (1.2)

--- edu.Guillermo.Garcia.Sainz/Reto5/ui/CLI.java
+++ edu.Guillermo.Garcia.Sainz/Reto5/ui/CLI.java
@@ -1,30 +1,40 @@
+import java.util.Scanner;
+
 public class CLI {
+
+    /*
+     * 
+     * Dentro de cada string estatico ire guardando las palabras,
+     * estas se guardaran en base a su dificultad
+     */
     
-    static string [] tablaFacil = {
-        {perro, gato, lapiz},
-        {taza, pluma, libro},
-        {cinta, gel, papel}
-    };
+    private static String tablaFacil = "1";
 
-    static string [] tablaIntermedia = {
-        {bateria, entrada, moneda},
-        {domino, ajedrez, pantalla},
-        {memoria, cinturon, guitarra}
-    };
+    private static String tablaIntermedia = "2";
 
-    static string [] tablaAvanzada = {
-        {computadora, composicion},
-        {bloqueador, interpretacion},
-        {receptor, multicontrol}
-    };
+    private static String tablaAvanzada = "3";
 
     public static void showJuego() {
+
         System.out.println("Bienvenido!");
         System.out.println("Ingrese la dificultad que desea escoger");
         System.out.println("/1/ Facil");
         System.out.println("/2/ Intermedio");
         System.out.println("/3/ Avanzado");
-        
-        
+
+        Scanner scanner1 = new Scanner(System.in);
+        int dificultad = scanner1.nextInt();
+
+        while (dificultad > 3 && dificultad < 1) {
+            System.out.println("Opccion invalida, porfavor intentalo de nuevo");
+
+            System.out.println("Bienvenido!");
+            System.out.println("Ingrese la dificultad que desea escoger");
+            System.out.println("/1/ Facil");
+            System.out.println("/2/ Intermedio");
+            System.out.println("/3/ Avanzado");
+
+            dificultad = scanner1.nextInt();
+        }
     }
 }
