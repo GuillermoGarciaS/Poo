07c2ec20101465c3b363d544f6ee06f886a1ec07
GuillermoGarciaS <eivengamer11roblox@gmail.com>
2023-02-25 23:46:28 -0600

    Reto5

---
+++ edu.Guillermo.Garcia.Sainz/src/Reto5/process/Juego.java
@@ -0,0 +1,44 @@
+package src.Reto5.process;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Juego {
+
+    public static String getOpccionFacil() {
+        List<String> Facil = Arrays.asList("perro", "gato", "lapiz"
+                                                ,"taza", "pluma", "libro"
+                                                ,"cinta", "gel", "papel");
+        Random random = new Random();
+        
+        int randomItem = random.nextInt(Facil.size());
+        String OpccionFacil = Facil.get(randomItem);
+        return OpccionFacil;
+    }
+
+    public static String getOpccionIntermedia() {
+        List<String> Intermedia = Arrays.asList ("bateria", "entrada", "moneda"
+                                                ,"domino", "ajedrez", "pantalla"
+                                                ,"memoria", "cinturon", "guitarra");
+        Random random = new Random();
+
+        int randomItem = random.nextInt(Intermedia.size());
+        String OpccionIntermedia = Intermedia.get(randomItem);  
+        return OpccionIntermedia;                                    
+    }
+
+    public static String getOpccionDificil() {
+        List<String> Dificil = Arrays.asList ("computadora", "composicion"
+                                             ,"bloqueador", "interpretacion"
+                                             ,"receptor", "multicontrol"
+                                             ,"multicontactos", "paracetamol"
+                                             ,"austrolopitecus");
+
+            Random random = new Random();
+
+            int randomItem = random.nextInt(Dificil.size());
+            String OpccionDificil = Dificil.get(randomItem);
+            return OpccionDificil; 
+    }
+}
