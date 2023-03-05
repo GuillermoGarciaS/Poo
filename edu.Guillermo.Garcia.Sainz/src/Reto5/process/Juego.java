e73a0abcc4d3c7502bdf32198d66109ea387c96c
GuillermoGarciaS <eivengamer11roblox@gmail.com>
2023-03-04 23:33:40 -0600

    Reto5 (Reto6 como tal)

--- edu.Guillermo.Garcia.Sainz/src/Reto5/process/Juego.java
+++ edu.Guillermo.Garcia.Sainz/src/Reto5/process/Juego.java
@@ -1,51 +1,169 @@
 package src.Reto5.process;
 
 import java.util.Arrays;
+import java.util.*;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Juego {
 
-    /*
-     * 
-     * Los public statics aqui los use para guardar las palabras
-     * asi como la operacion para que se escoga una aleatoriamente
-     */
+    public static void getModoFacil () {
 
-    public static String getOpccionFacil() {
+        /*
+         * Para esta parte me atore, se puede notar que es un tanto diferente
+         * a la version que tenía antes, le pedí ayuda a Lucilel
+         */
 
-        List<String> Facil = Arrays.asList("perro", "gato", "lapiz"
-                                                ,"taza", "pluma", "libro"
-                                                ,"cinta", "gel", "papel");
+        List<String> faciles = Arrays.asList("perro", "gato", "lapiz"
+                                                   ,"taza", "pluma", "libro"
+                                                   ,"cinta", "gel", "pape");
         Random random = new Random();
-        
-        int randomItem = random.nextInt(Facil.size());
-        String OpccionFacil = Facil.get(randomItem);
-        return OpccionFacil;
+        int palabraRandom = random.nextInt(faciles.size());
+        String palabraFacil = faciles.get(palabraRandom);
+
+        ArrayList<String> letrasIntroducidas = new ArrayList<>();
+
+        ArrayList<Integer> letrasAdivinadas = new ArrayList<>();
+
+        String letraYaIntroducida;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Adivine la siguiente palabra: ");
+
+        while (letrasAdivinadas.size() != palabraFacil.length()) {
+
+            boolean letraAdivinada = false;
+            letraYaIntroducida = scanner.next().toLowerCase();
+
+            for (int i = 0; i < palabraFacil.length(); i++) {
+
+                if (palabraFacil.charAt(i) == letraYaIntroducida.charAt(0)) {
+                    letraAdivinada = true;
+                    letrasAdivinadas.add(i);
+
+                }
+            }
+            if (letraAdivinada) {
+                letrasIntroducidas.add(letraYaIntroducida);
+
+                for (int i = 0; i < palabraFacil.length(); i++) {
+                    if (letrasAdivinadas.contains(i)) {
+                        System.out.println(palabraFacil.charAt(i));
+                    }
+                    else {
+                        System.out.println("_");
+                    }
+                }
+                System.out.println();
+            }
+            else {
+                letrasIntroducidas.add(letraYaIntroducida);
+                System.out.println("Letra incorrecta! intentalo de nuevo");
+            }
+        }
     }
 
-    public static String getOpccionIntermedia() {
-        List<String> Intermedia = Arrays.asList ("bateria", "entrada", "moneda"
-                                                ,"domino", "ajedrez", "pantalla"
-                                                ,"memoria", "cinturon", "guitarra");
-        Random random = new Random();
+    public static void getModoIntermedio() {
 
-        int randomItem = random.nextInt(Intermedia.size());
-        String OpccionIntermedia = Intermedia.get(randomItem);  
-        return OpccionIntermedia;                                    
+                List<String> intermedias = Arrays.asList("bateria", "entrada", "moneda"
+                                                              ,"domino", "ajedrez", "pantalla"
+                                                              ,"memoria", "cinturon", "guitarra");
+        Random random = new Random();
+        int palabraRandom = random.nextInt(intermedias.size());
+        String palabraIntermedia = intermedias.get(palabraRandom);
+
+        ArrayList<String> letrasIntroducidas = new ArrayList<>();
+
+        ArrayList<Integer> letrasAdivinadas = new ArrayList<>();
+
+        String letraYaIntroducida;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Adivine la siguiente palabra: ");
+
+        while (letrasAdivinadas.size() != palabraIntermedia.length()) {
+
+            boolean letraAdivinada = false;
+            letraYaIntroducida = scanner.next().toLowerCase();
+
+            for (int i = 0; i < palabraIntermedia.length(); i++) {
+
+                if (palabraIntermedia.charAt(i) == letraYaIntroducida.charAt(0)) {
+                    letraAdivinada = true;
+                    letrasAdivinadas.add(i);
+
+                }
+            }
+            if (letraAdivinada) {
+                letrasIntroducidas.add(letraYaIntroducida);
+
+                for (int i = 0; i < palabraIntermedia.length(); i++) {
+                    if (letrasAdivinadas.contains(i)) {
+                        System.out.println(palabraIntermedia.charAt(i));
+                    }
+                    else {
+                        System.out.println("_");
+                    }
+                }
+                System.out.println();
+            }
+            else {
+                letrasIntroducidas.add(letraYaIntroducida);
+                System.out.println("Letra incorrecta! intentalo de nuevo");
+            }
+        }
     }
 
-    public static String getOpccionDificil() {
-        List<String> Dificil = Arrays.asList ("computadora", "composicion"
-                                             ,"bloqueador", "interpretacion"
-                                             ,"receptor", "multicontrol"
-                                             ,"multicontactos", "paracetamol"
-                                             ,"austrolopitecus");
+    public static void getModoDificil() {
 
-            Random random = new Random();
+                List<String> dificiles = Arrays.asList("bateria", "entrada", "moneda"
+                                                              ,"domino", "ajedrez", "pantalla"
+                                                              ,"memoria", "cinturon", "guitarra");
+        Random random = new Random();
+        int palabraRandom = random.nextInt(dificiles.size());
+        String palabraDificil = dificiles.get(palabraRandom);
 
-            int randomItem = random.nextInt(Dificil.size());
-            String OpccionDificil = Dificil.get(randomItem);
-            return OpccionDificil; 
+        ArrayList<String> letrasIntroducidas = new ArrayList<>();
+
+        ArrayList<Integer> letrasAdivinadas = new ArrayList<>();
+
+        String letraYaIntroducida;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Adivine la siguiente palabra: ");
+
+        while (letrasAdivinadas.size() != palabraDificil.length()) {
+
+            boolean letraAdivinada = false;
+            letraYaIntroducida = scanner.next().toLowerCase();
+
+            for (int i = 0; i < palabraDificil.length(); i++) {
+
+                if (palabraDificil.charAt(i) == letraYaIntroducida.charAt(0)) {
+                    letraAdivinada = true;
+                    letrasAdivinadas.add(i);
+
+                }
+            }
+            if (letraAdivinada) {
+                letrasIntroducidas.add(letraYaIntroducida);
+
+                for (int i = 0; i < palabraDificil.length(); i++) {
+                    if (letrasAdivinadas.contains(i)) {
+                        System.out.println(palabraDificil.charAt(i));
+                    }
+                    else {
+                        System.out.println("_");
+                    }
+                }
+                System.out.println();
+            }
+            else {
+                letrasIntroducidas.add(letraYaIntroducida);
+                System.out.println("Letra incorrecta! intentalo de nuevo");
+            }
+        }
     }
 }
+
--- edu.Guillermo.Garcia.Sainz/src/Reto5/ui/CLI.java
+++ edu.Guillermo.Garcia.Sainz/src/Reto5/ui/CLI.java
@@ -2,134 +2,70 @@ package src.Reto5.ui;
 
 import src.Reto5.process.*;
 
-import java.util.ArrayList;
 import java.util.Scanner;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+
+import src.Reto5.menu.*;
+
 
 public class CLI {
 
-    /*
-     * 
-     * Dentro de cada string estatico ire guardando las palabras,
-     * estas se guardaran en base a su dificultad
-     */
+      private static String modoFacil = "|Modo Facil|";
+      private static String seleccionarModoFacil = "Facil";
 
-    public static void showJuego() {
+      private static String modoIntermedio = "|Modo Intermedio|";
+      private static String seleccionarModoIntermedio = "Intermedio";
 
-        ArrayList<String> letrasAgregadas = new ArrayList<>();
-        ArrayList<Integer> letrasDescubiertas = new ArrayList<>();
-        String palabra = "";
-        String letraYaIntroducida;
-        int dificultad = 5;
-        int opccionFinal;
+      private static String modoDificil = "|Modo Dificil|";
+      private static String seleccionarModoDificil = "Dificil";
 
-        while (dificultad != 0) {
-            letrasAgregadas.clear();
-            letrasDescubiertas.clear();
 
-            System.out.println("Bienvenido!");
-            System.out.println("Ingrese la dificultad que desea escoger");
-            System.out.println("|1| Facil");
-            System.out.println("|2| Intermedio");
-            System.out.println("|3| Avanzado");
-            System.out.println("|0| Salir");
-    
-            Scanner scanner = new Scanner(System.in);
-            dificultad = scanner.nextInt();
-    
-    
-            while (dificultad > 3 && dificultad < 1 ) {
-                System.out.println("Opccion invalida, porfavor intentalo de nuevo");
-    
-                System.out.println("Bienvenido!");
-                System.out.println("Ingrese la dificultad que desea escoger");
-                System.out.println("|1| Facil");
-                System.out.println("|2| Intermedio");
-                System.out.println("|3| Avanzado");
-    
-                dificultad = scanner.nextInt();
-            }
-    
-            /*
-             * 
-             * aqui uso un switch para que llame a escoger el modo
-             * de dificultad en base a un numero ya marcado en los prints 
-             * localizados arriba
-             */
-    
-            switch (dificultad) {
-                case 1:
-                palabra = Juego.getOpccionFacil();
-                break;
-                case 2:
-                palabra = Juego.getOpccionIntermedia();
-                break;
-                case 3:
-                palabra = Juego.getOpccionDificil();
-                break;
-            }
-    
-            /*
-             * 
-             * aqui vamos a evitar algunos errores que el usuario quizas cometa,
-             * como el de letras repetidas, así como el poder evitar que 
-             * 
-             */
-    
-            while (letrasDescubiertas.size() != palabra.length()) {
-                System.out.println("Intruduzca su letra");
-                letraYaIntroducida = scanner.next().toUpperCase();
-    
-                if (letrasAgregadas.contains(letraYaIntroducida)) {
-                    System.out.println("Ya introdujo esta letra");
-                    continue;
-                }
-    
-                boolean letrasEncontradas = false;
-                for (int i =0; i < palabra.length(); i++); {
-                    if (palabra.charAt('i') == letraYaIntroducida.charAt(0)) {
-                        letrasEncontradas = true;
-                        letrasDescubiertas.add(i);
-                    }
-                }
-    
-                if (letrasEncontradas) {
-                    letrasAgregadas.add(letraYaIntroducida);
-    
-                    for (int i = 0; i < palabra.length(); i++); {
-                        if (letrasEncontradas.contains(i)) {
-                            System.out.println(palabra.charAt('i'));
-                        }else {
-                            System.out.println("_");
-                        }
-                    }
-                    System.out.println();
-                }else {
-                    letrasAgregadas.add(letraYaIntroducida);
-                    System.out.println("Intente de nuevo");
-                }
-            }
-            System.out.println("Desea continuar?");
-            System.out.println("|1| para continuar");
-            System.out.println("|0| para salir");
+      private static Menu menu = new Menu();
+      private static MaximizeAction easyAction = (scanner) -> {
+            System.out.println(modoFacil);
+            String palabraFacil = "";
+            palabraFacil = Juego.getModoFacil();
+            System.out.println(palabraFacil);
+            System.out.println();
 
-            Scanner scannerFinal = new Scanner(System.in);
-            opccionFinal = scannerFinal.nextInt();
+      
+      };
 
-            if (opccionFinal == 1) {
-                System.out.println("Entendido, iniciando de nuevo el proyecto...");
+      private static MaximizeAction intermediumAction = (scanner) -> {
+            System.out.println(modoFacil);
+            String palabraIntermedia = "";
+            palabraIntermedia = Juego.getModoIntermedio();
+            System.out.println(palabraIntermedia);
+            System.out.println();
+      };
 
-                System.out.println("Bienvenido!");
-                System.out.println("Ingrese la dificultad que desea escoger");
-                System.out.println("|1| Facil");
-                System.out.println("|2| Intermedio");
-                System.out.println("|3| Avanzado");
-    
-                dificultad = scanner.nextInt();
+      private static MaximizeAction HardAction = (scanner) -> {
+            System.out.println(modoDificil);
+            String palabraDificil = "";
+            palabraDificil = Juego.getModoDificil();
+            System.out.println(palabraDificil);
+            System.out.println();
+      }; 
 
-                } else {
-                    System.out.println("Gracias por jugar! tenga un buen día");
-                    break;
-                }
-            }
-         }
+      static { 
+            menu
+            .agregarOpccion(seleccionarModoFacil, easyAction);
+
+            menu
+            .agregarOpccion(seleccionarModoIntermedio, intermediumAction);
+
+            menu
+            .agregarOpccion(seleccionarModoDificil, HardAction);
+
+            menu
+            .crearMenu();
       }
+
+      public static void showJuego(){
+            menu.showMenu();
+            var option = menu.readOption();
+            menu.seleccionarYCorrerOpcion(option);
+      }while (menu.isAlive());
+  }
+
